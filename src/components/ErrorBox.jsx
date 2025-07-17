59ef5d89bf7de1dd23402c7c2ba7419d79b2d571
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-07-17 17:43:16 +0530

    changed error box ui

--- src/components/ErrorBox.jsx
+++ src/components/ErrorBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ErrorBox = ({error}) => {
     return (
         <div
-            className="text-red-600 bg-white border-2 border-red-400 rounded-lg px-4 py-2 max-w-md w-[10    0%] text-center font-semibold shadow-sm transition-all animate-fade-in">
+            className="text-red-600 bg-white border-2 border-red-400 rounded-lg px-4 max-w-xs self-center py-2 text-center font-semibold shadow-sm transition-all animate-fade-in">
             {error}
         </div>
     )
