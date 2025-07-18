f14a7774ae220c143b8a1f137ac8f3c0ab6b8c25
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-07-18 16:40:50 +0530

    removes timeout from axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
-    timeout: 10000, // 10sec
+    // timeout: 10000, // 10sec
     withCredentials: true, // Allow cookies to be sent with requests
 });
 
